20836d9db41d257d3c89fb6755fe3fb34ae323a0
Sebastian Lemke <skeeve55@gmx.de>
2013-06-02 22:54:16 +0200

    PK and Creation for DbUserVote

--- service/data/__init__.py
+++ service/data/__init__.py
@@ -31,3 +31,4 @@ class DbUserVote(Base):
     __tablename__ = 'user_votes'
     user_id = Column(Integer, ForeignKey("users.id"), primary_key=True)
     vote_id = Column(Integer, ForeignKey("votes.id"), primary_key=True)
+    creation = Column(Text) 
