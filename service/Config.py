fd2ced378b49472153f197432102bb6e71da23e2
Aggravate <steffen.so@gmail.com>
2013-06-03 11:42:07 +0200

    refactor folders

---
+++ service/Config.py
@@ -0,0 +1,19 @@
+class Config:
+    config = {}
+    config['db_schema'] = "mysql" 
+    config['db_user'] = "root" 
+    config['db_password'] = ""
+    config['db_host'] = "127.0.0.1"
+    config['db_port'] = "3306"
+    config['db_database'] = "pollit"
+        
+    config['flask_host'] = "localhost"
+
+    def get_db_connection_strin(self):
+        if self.config['db_password'] == "":
+            return "%s://%s@%s:%s/%s" % (self.config['db_schema'], self.config['db_user'], self.config['db_host'], self.config['db_port'], self.config['db_database'])
+        else :
+            return "%s://%s:%s@%s:%s/%s" % (self.config['schema'], self.config['user'], self.config['password'], self.config['host'], self.config['port'], self.config['database'])
+        
+    def get_flask_host(self):
+        return self.config['flask_host'] 
--- service/main.py
+++ service/main.py
@@ -3,20 +3,12 @@ import flask  # @UnresolvedImport
 from flask.ext.sqlalchemy import SQLAlchemy  # @UnresolvedImport
 
 from DataAccess import DataAccess
+from Config import Config
 
-config = {}
-config['schema'] = "mysql" 
-config['user'] = "root" 
-config['password'] = ""
-config['host'] = "127.0.0.1"
-config['port'] = "3306"
-config['database'] = "pollit"
-
-dbConnectionString = "%s://%s@%s:%s/%s" % (config['schema'], config['user'], config['host'], config['port'], config['database'])
-#dbConnectionString = "%s://%s:%s@%s:%s/%s" % (config['schema'], config['user'], config['password'], config['host'], config['port'], config['database'])
+config = Config()
 
 app = flask.Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = dbConnectionString
+app.config['SQLALCHEMY_DATABASE_URI'] = config.get_db_connection_strin()
 app.debug = True
 
 db = SQLAlchemy(app)
@@ -41,5 +33,5 @@ def return_all_polls_as_jason():
     return flask.jsonify({"polls" : dataAccess.get_all_polls()})
 
 if __name__ == "__main__":
-    app.run(host="localhost")
+    app.run(host=config.get_flask_host())
 
