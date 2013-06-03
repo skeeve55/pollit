d1cfc785ad6d712f2b0475426c5871809e738f73
Aggravate <steffen.so@gmail.com>
2013-06-03 13:16:45 +0200

    refactor folders

--- service/FlaskApp.py
+++ service/FlaskApp.py
@@ -1,15 +1,24 @@
 import flask  # @UnresolvedImport
 from flask.ext.sqlalchemy import SQLAlchemy  # @UnresolvedImport
 
+from Config import Config
+from DataAccess import DataAccess
 
 class FlaskApp:
-    def __init__(self, connectionString, host):
+    def __init__(self):
+        self.config = Config()
+        
         self.app = flask.Flask(__name__)
-        self.app.config['SQLALCHEMY_DATABASE_URI'] = connectionString        
-        self.app.config['POLLIT_SERVER_NAME'] = host        
+        self.app.config['SQLALCHEMY_DATABASE_URI'] = self.config.get_db_connection_string()       
+        self.app.config['POLLIT_SERVER_NAME'] = self.config.get_flask_host()
         self.app.debug = True
         
         self.db = SQLAlchemy(self.app)
+        self.dataAccess = DataAccess(self.db)
+        
+        self.app.add_url_rule('/', 'return_all_polls_as_html', self.return_all_polls_as_html)
+        self.app.add_url_rule('/polls', 'return_all_polls_as_jason', self.return_all_polls_as_jason)
+        self.app.add_url_rule('/test', 'return_test', self.return_test)
         
     def run(self):
         self.app.run(host=self.app.config['POLLIT_SERVER_NAME'])
@@ -18,8 +27,18 @@ class FlaskApp:
         return self.db
     
     def get_app(self):
-        return self.app
+        return self.app    
     
-    @self.app.route('/test2/')
-    def test(self):
-        return "hello, world"
+    def return_all_polls_as_html(self):
+        return flask.render_template('index.html', polls = self.dataAccess.get_all_polls() )
+    
+    def return_test(self):
+        try:
+            self.dataAccess.insert_user_vote(1, 2);
+        except: 
+            self.db.session.rollback()
+        finally:    
+            return flask.jsonify({"count" : self.dataAccess.get_user_votes_for_vote(2)})
+            
+    def return_all_polls_as_jason(self):
+        return flask.jsonify({"polls" : self.dataAccess.get_all_polls()})
--- service/main.py
+++ service/main.py
@@ -1,32 +1,6 @@
-import flask  # @UnresolvedImport
-from flask.ext.sqlalchemy import SQLAlchemy  # @UnresolvedImport
-
-from Config import Config
-from DataAccess import DataAccess
 from FlaskApp import FlaskApp
 
-config = Config()
-flaskApp = FlaskApp(config.get_db_connection_string(), config.get_flask_host())
-app = flaskApp.get_app()
-db = flaskApp.get_db()
-dataAccess = DataAccess(db)
-
-@app.route('/test/')
-def return_test():
-    try:
-        dataAccess.insert_user_vote(1, 2);
-    except: 
-        db.session.rollback()
-    finally:    
-        return flask.jsonify({"count" : dataAccess.get_user_votes_for_vote(2)})
-        
-@app.route("/")
-def return_all_polls_as_html():
-    return flask.render_template('index.html', polls = dataAccess.get_all_polls() )
-
-@app.route("/polls")
-def return_all_polls_as_jason():
-    return flask.jsonify({"polls" : dataAccess.get_all_polls()})
+flaskApp = FlaskApp()
 
 if __name__ == "__main__":
     flaskApp.run()
