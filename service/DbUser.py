adafaed221ea935e475c6d0471aa74905a7e9e75
Aggravate <steffen.so@gmail.com>
2013-06-03 13:43:01 +0200

    clean up

--- service/DbUser.py
+++
@@ -1,8 +0,0 @@
-from DbBase import Base
-from sqlalchemy.schema import Column
-from sqlalchemy.types import Text, Integer
-
-class DbUser(Base):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    username = Column(Text)  
--- service/DbUserVote.py
+++
@@ -1,9 +0,0 @@
-from DbBase import Base
-from sqlalchemy.schema import Column, ForeignKey
-from sqlalchemy.types import Text, Integer
-
-class DbUserVote(Base):
-    __tablename__ = 'user_votes'
-    user_id = Column(Integer, ForeignKey("users.id"), primary_key=True)
-    vote_id = Column(Integer, ForeignKey("votes.id"), primary_key=True)
-    creation = Column(Text) 
